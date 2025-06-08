6ffa7197d3ba9c84071cffb95d1eb03d48d540e2
Leoogata <leonardo.pedrosa@sou.inteli.edu.br>
2025-06-08 16:32:49 -0300

    first commit

---
+++ js/sketch.js
@@ -0,0 +1,35 @@
+let vwLogo;
+
+function preload() {
+  // Carrega o modelo STL
+  vwLogo = loadModel('../assets/vw_logo.stl', true);
+}
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+}
+
+function draw() {
+    background(30);
+
+    // Iluminação
+    directionalLight(255, 255, 255, 0.5, 0.5, -1);
+    pointLight(255, 255, 255, 200, -200, 300);
+    ambientLight(60);
+
+    // Material com reflexo
+    ambientMaterial(255); // Azul metálico
+
+    // Posicionamento no centro da cena
+    translate(1, 0, 0);
+
+    rotateX(PI);
+    // Rotação automática no eixo Y
+    rotateY(frameCount * -0.02);
+
+    // Exibe o modelo com escala adequada
+    scale(1); // Ajuste conforme necessário
+    model(vwLogo);
+}
+
+
